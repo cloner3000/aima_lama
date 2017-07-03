6db8c663497fb38f8a86c0c0c681b827c6713691
Halim Tuhu Prasetyo <halimtuhuprasetyo@gmail.com>
2017-07-03 12:01:11 +0700

    standar2 jurusan ke prodi

--- php/app/Http/Controllers/Standar2Controller.php
+++ php/app/Http/Controllers/Standar2Controller.php
@@ -8,7 +8,7 @@ use App\Standar2;
 class Standar2Controller extends Controller
 {
   public function index(){
-    $standar2 = Standar2::where("id_jurusan", 1)->get();
+    $standar2 = Standar2::where("id_prodi", 1)->get();
     $data=array();
     foreach ($standar2 as $data_standar2) {
       $data[$data_standar2->kode]=$data_standar2->nilai;
@@ -20,9 +20,9 @@ class Standar2Controller extends Controller
   public function save(Request $request){
     $timestamp = date("Y-m-d H:i:s");
     $data=array(
-      array("kode"=>"2.1", "nilai"=>$request->skor2_1, "id_jurusan"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
-      array("kode"=>"2.2", "nilai"=>$request->skor2_2, "id_jurusan"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
-      array("kode"=>"2.6", "nilai"=>$request->skor2_6, "id_jurusan"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
+      array("kode"=>"2.1", "nilai"=>$request->skor2_1, "id_prodi"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
+      array("kode"=>"2.2", "nilai"=>$request->skor2_2, "id_prodi"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
+      array("kode"=>"2.6", "nilai"=>$request->skor2_6, "id_prodi"=>1, "created_at"=>$timestamp, "updated_at"=>$timestamp),
       );
 
     $standar2 = Standar2::insert($data);
